26c6d5508078d3c0acf802cc6482cc5c537a79a1
beskep <beskep@naver.com>
2024-10-31 14:21:37 +0900

    feat: public ami cpr by time

--- scripts/ami/public_building.py
+++ scripts/ami/public_building.py
@@ -1,18 +1,18 @@
 from __future__ import annotations
 
 import dataclasses as dc
+from functools import lru_cache
 from io import StringIO
 from pathlib import Path
-from typing import TYPE_CHECKING, Literal, NamedTuple
+from typing import TYPE_CHECKING, Annotated, Literal, NamedTuple
 
 import matplotlib.pyplot as plt
 import polars as pl
 import rich
 import seaborn as sns
-from cyclopts import App
+from cyclopts import App, Parameter
 from loguru import logger
 from polars.exceptions import ComputeError
-from rich.progress import track
 
 from greenbutton import cpr, utils
 from scripts.config import Config
@@ -209,122 +209,182 @@ def ami_plot(src: Path | None = None, dst: Path | None = None):
     utils.MplTheme().grid().apply()
     utils.MplConciseDate().apply()
 
-    for building in track(buildings):
-        fig = _ami_plot(lf.filter(pl.col('기관ID') == building))
-        fig.savefig(dst / f'{building}.png')
-        plt.close(fig)
+    with utils.Progress() as p:
+        for building in p.track(buildings):
+            fig = _ami_plot(lf.filter(pl.col('기관ID') == building))
+            fig.savefig(dst / f'{building}.png')
+            plt.close(fig)
 
 
 class Building(NamedTuple):
+    idx: str
     id_: str
     name: str
     area: float
     region: str
 
-
-@dc.dataclass
-class PublicAmiCpr:
-    energy: Literal['사용량', '보정사용량'] = '보정사용량'
-
-    conf: _Config = dc.field(default_factory=_Config)
-    building_file: str = '1.기관-주소변환.parquet'
-    temperature_file: str = 'temperature.parquet'
-
-    def building_count(self):
+    def __str__(self):
         return (
-            pl.scan_parquet(self.conf.data / self.building_file)
-            .select(pl.len())
-            .collect()
-            .item()
+            f'{type(self).__name__}'
+            f'(idx={self.idx}, name={self.name!r}, region={self.region!r})'
         )
 
-    def iter_building(self):
-        """
-        Iterate `Building`.
 
-        Yields
-        ------
-        Building
-        """
+@dc.dataclass
+class PublicAMI:
+    energy: Literal['사용량', '보정사용량'] = '보정사용량'
+    institution: str | None = None
+
+    conf: _Config = dc.field(default_factory=_Config)
+    _file_bldg: str = '1.기관-주소변환.parquet'
+    _file_temp: str = 'temperature.parquet'
+
+    def buildings(self):
+        lf = (
+            pl.scan_parquet(self.conf.data / self._file_bldg)
+            .with_row_index()
+            .with_columns(pl.col('index').cast(pl.String).str.pad_start(4, '0'))
+        )
+
+        if self.institution:
+            lf = lf.filter(pl.col('기관대분류') == self.institution)
+        return lf
+
+    def building_count(self) -> int:
+        return self.buildings().select(pl.len()).collect().item()
+
+    def iter_building(self):
         for row in (
-            pl.scan_parquet(self.conf.data / self.building_file)
-            .select('기관ID', '기관명', '연면적', 'asos_code')
+            self.buildings()
+            .select('index', '기관ID', '기관명', '연면적', 'asos_code')
+            .sort('asos_code')
             .collect()
             .iter_rows()
         ):
             yield Building(*row)
 
-    def ami(self, institution_id: str):
+    def ami(self, iid: str):
         return (
             pl.scan_parquet(list(self.conf.data.glob('PublicAMI*.parquet')))
-            .filter(pl.col('기관ID') == institution_id)
+            .filter(pl.col('기관ID') == iid)
             .with_columns()
         )
 
     def temperature(self, region: str):
         return (
-            pl.scan_parquet(self.conf.weather / self.temperature_file)
+            pl.scan_parquet(self.conf.weather / self._file_temp)
             .filter(pl.col('region2') == region)
-            .select('datetime', 'ta')
+            .select('datetime', pl.col('ta').alias('temperature'))
         )
 
-    def path(
-        self,
-        building: Building,
-        weekday: Literal['weekday', 'weekend', None] = None,
-    ):
-        w = '' if weekday is None else f' {weekday}'
-        name = f'{building.id_}({building.name}){w}'
-        return self.conf.cpr / self.energy / f'{name}.ext'
+    def data(self, building: Building, temperature: pl.DataFrame | None = None):
+        # XXX LocalOutlierFactor 이상치 제거?
+        temp = (
+            self.temperature(building.region).collect()
+            if temperature is None
+            else temperature
+        )
+        ami = (
+            self.ami(building.id_)
+            .select(
+                'datetime',
+                pl.col(self.energy).truediv(building.area).alias('energy'),
+            )
+            .collect()
+        )
+        data = ami.join(temp, on='datetime', how='inner').drop_nulls()
 
-    def cpr(
-        self,
-        building: Building,
-        weekday: Literal['weekday', 'weekend', None] = None,
-    ):
+        return data.select(
+            'datetime',
+            pl.col('datetime').dt.year().alias('year'),
+            pl.col('datetime').dt.weekday().is_in([6, 7]).alias('is_weekend'),
+            pl.col('datetime').dt.hour().alias('hour'),
+            'temperature',
+            'energy',
+        )
+
+    def __iter__(self):
+        @lru_cache
+        def read_temp(region: str):
+            return self.temperature(region=region).collect()
+
+        for bldg in self.iter_building():
+            temp = read_temp(region=bldg.region)
+            yield bldg, self.data(building=bldg, temperature=temp)
+
+    def track(self):
+        with utils.Progress() as p:
+            yield from p.track(self, total=self.building_count())
+
+
+@dc.dataclass
+class PublicAmiCpr:
+    ami: PublicAMI
+
+    week: Literal['weekday', 'weekend', 'week'] = 'week'
+    hour: Literal['day', '24h'] = 'day'
+    day_bound: tuple[int, int] = (9, 18)
+
+    style: cpr.PlotStyle = dc.field(  # type: ignore[assignment]
+        default_factory=lambda: {
+            'scatter': {'alpha': 0.20, 'hue': 'year', 'palette': 'crest'}
+        }
+    )
+
+    def suffix(self):
+        hour = (
+            f'{self.hour}({self.day_bound[0]}-{self.day_bound[1]})'
+            if self.hour == 'day'
+            else self.hour
+        )
+        return f'{self.week}_{hour}'
+
+    def path(self, building: Building):
+        return (
+            self.ami.conf.cpr / f'{building.idx}_{building.name}_'
+            f'{self.ami.energy}_{self.suffix()}.ext'
+        )
+
+    def cpr(self, building: Building, data: pl.DataFrame | None = None):
         if building.area == 0:
             raise ZeroDivisionError
 
-        ami = self.ami(building.id_).select(
-            'datetime', pl.col(self.energy).truediv(building.area).alias('energy')
-        )
-        temperature = self.temperature(building.region).rename({'ta': 'temperature'})
-        data = (
-            ami.join(temperature, on='datetime', how='inner')
-            .with_columns(pl.col('datetime').dt.year().alias('year'))
-            .drop_nulls()
-            .collect()
-        )
+        if data is None:
+            data = self.ami.data(building=building)
+
+        if self.week is not None:
+            data = data.filter(
+                pl.when(self.week == 'weekend')
+                .then(pl.col('is_weekend'))
+                .otherwise(pl.col('is_weekend').not_())
+            )
+        if self.hour == 'day':
+            data = data.filter(pl.col('hour').is_between(*self.day_bound))
 
         if not data.height:
             raise EmptyDataError
 
-        if weekday is not None:
-            days = [1, 2, 3, 4, 5] if weekday == 'weekday' else [6, 7]
-            data = data.filter(pl.col('datetime').dt.weekday().is_in(days))
+        return cpr.ChangePointRegression(data).optimize_multi_models()
 
-        reg = cpr.ChangePointRegression(data)
-        return reg.optimize_multi_models()
-
-    def cpr_and_write(
+    def save_cpr(
         self,
         building: Building,
-        weekday: Literal['weekday', 'weekend', None] = None,
+        data: pl.DataFrame | None = None,
         *,
         skip_exists: bool = False,
     ):
-        path = self.path(building=building, weekday=weekday)
-        if skip_exists and path.with_suffix('.xlsx').exists():
-            return
+        path = self.path(building=building)
+        if skip_exists and path.with_suffix('.png').exists():
+            return None
 
-        model = self.cpr(building, weekday=weekday)
-        model.model_frame.write_excel(path.with_suffix('.xlsx'))
+        model = self.cpr(building=building, data=data)
 
         fig, ax = plt.subplots()
-        model.plot(
-            ax=ax, style={'scatter': {'alpha': 0.25, 'hue': 'year', 'palette': 'crest'}}
+        model.plot(ax=ax, style=self.style)
+        ax.set_title(
+            f'{building.name} {self.suffix()} (r²={model.model_dict["r2"]:.4g})',
+            loc='left',
         )
-
         ax.update_datalim([[0, 0]], updatex=False, updatey=True)
         ax.autoscale_view()
         ax.set_xlabel('기온 [℃]')
@@ -334,38 +394,56 @@ class PublicAmiCpr:
             if h is not None:
                 h.set_alpha(0.8)
 
+        if not model.is_valid:
+            ax.set_facecolor('coral')
+
         fig.savefig(path.with_suffix('.png'))
         plt.close(fig)
 
-    def _iter(self):
-        with utils.Progress() as p:
-            yield from p.track(self.iter_building(), total=self.building_count())
+        return model.model_frame
 
-    def batch_cpr(self):
-        bldg = next(self.iter_building())
-        path = self.path(bldg, 'weekday')
-        path.parent.mkdir(exist_ok=True)
+    def batch_cpr(self, *, skip_exists: bool = False):
+        dfs: list[pl.DataFrame] = []
 
-        for building in self._iter():
-            logger.info(building)
+        for bldg, data in self.ami:
+            logger.info(bldg)
 
-            kwargs = {'building': building, 'skip_exists': True}
             try:
-                self.cpr_and_write(weekday='weekday', **kwargs)
-                self.cpr_and_write(weekday='weekend', **kwargs)
-            except (EmptyDataError, ZeroDivisionError) as e:
-                logger.warning(e.__class__.__name__)
+                df = self.save_cpr(building=bldg, data=data, skip_exists=skip_exists)
+            except EmptyDataError:
+                logger.warning('Empty Data')
                 continue
+
+            if df is not None:
+                dfs.append(
+                    df.select(
+                        pl.lit(bldg.idx).alias('index'),
+                        pl.lit(bldg.id_).alias('id'),
+                        pl.lit(bldg.name).alias('name'),
+                        pl.all(),
+                    )
+                )
+
+        pl.concat(dfs).write_excel(
+            self.ami.conf.cpr / f'[model] {self.ami.energy}_{self.suffix()}.xlsx'
+        )
 
 
 @app.command
-def public_cpr(energy: Literal['사용량', '보정사용량'] = '보정사용량'):
-    pa = PublicAmiCpr(energy=energy)
-    pa.conf.cpr.mkdir(exist_ok=True)
+def public_cpr(
+    energy: Literal['사용량', '보정사용량'] = '사용량',
+    institution: Annotated[str, Parameter(negative='--all')] = '정부청사관리',
+    week: Literal['weekday', 'weekend', 'week'] = 'week',
+    hour: Literal['day', '24h'] = 'day',
+    day_bound: tuple[int, int] = (9, 18),
+):
+    ami = PublicAMI(energy=energy, institution=institution)
+    pa = PublicAmiCpr(ami=ami, week=week, hour=hour, day_bound=day_bound)
+    pa.ami.conf.cpr.mkdir(exist_ok=True)
     pa.batch_cpr()
 
 
 if __name__ == '__main__':
     utils.set_logger()
-    utils.MplTheme().grid().apply()
+    utils.MplTheme('paper').grid().apply()
     app()
