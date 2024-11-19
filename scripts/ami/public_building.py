1598c633a52fa00340cbd57234a015cb71adc9c8
beskep <beskep@naver.com>
2024-11-19 20:05:17 +0900

    feat: 공공건물 CPR 분석 옵션

--- scripts/ami/public_building.py
+++ scripts/ami/public_building.py
@@ -23,10 +23,6 @@ if TYPE_CHECKING:
     from matplotlib.axes import Axes
 
 
-class EmptyDataError(ValueError):
-    pass
-
-
 @dc.dataclass
 class _Config:
     path: dc.InitVar[str | Path] = 'config/config.toml'
@@ -35,6 +31,7 @@ class _Config:
         '02data',
         '03EDA',
         '20CPR',
+        '40analysis',
         '99weather',
     )
 
@@ -45,21 +42,24 @@ class _Config:
     data: Path = dc.field(init=False)
     eda: Path = dc.field(init=False)
     cpr: Path = dc.field(init=False)
+    analysis: Path = dc.field(init=False)
     weather: Path = dc.field(init=False)
 
     def __post_init__(self, path: str | Path, subdirs: Sequence[str]):
         conf = Config.read(Path(path))
         self.root = conf.ami.root / 'Public'
 
-        attributes = ['raw', 'data', 'eda', 'cpr', 'weather']
+        attributes = ['raw', 'data', 'eda', 'cpr', 'analysis', 'weather']
         for attr, subdir in zip(attributes, subdirs, strict=True):
             setattr(self, attr, self.root / subdir)
 
 
 app = App()
+app.command(App('ami'))
+app.command(App('cpr'))
 
 
-@app.command
+@app['ami'].command
 def building_info(src: Path | None = None, dst: Path | None = None):
     conf = _Config()
     src = src or conf.raw
@@ -89,7 +89,7 @@ def building_info(src: Path | None = None, dst: Path | None = None):
         )
 
 
-@app.command
+@app['ami'].command
 def address():
     """
     주소 표준화 자료 검토, 저장.
@@ -138,11 +138,46 @@ def address():
     rich.print('지역=', region, sep='')
 
 
-@app.command
-def ami(src: Path | None = None, dst: Path | None = None):
+@app['ami'].command
+def elec_equipment():
+    """전기식 설비 통계."""
     conf = _Config()
-    src = src or conf.raw
-    dst = dst or conf.data
+
+    bldg = (
+        pl.scan_parquet(conf.data / '1.기관-주소변환.parquet')
+        .select('기관ID', '기관명', '기관대분류', '건물용도', 'asos_code')
+        .collect()
+    )
+    equipment = pl.read_csv(
+        conf.raw / '3.냉난방방식.txt',
+        separator='|',
+        encoding='korean',
+    )
+    cols = equipment.columns
+    equipment = (
+        equipment.drop_nulls()
+        .drop(cols[3])  # 계산 오류
+        .filter(pl.col(cols[1]) != 0)
+        .with_columns(
+            (
+                pl.col(cols[1])  # br
+                / (pl.col(cols[1]) + pl.col(cols[2]))
+            ).alias('전기식용량비율')
+        )
+        .sort('전기식용량비율', descending=True)
+        .join(bldg, on='기관ID', how='left')
+    )
+
+    equipment.write_parquet(conf.data / '냉난방방식.parquet')
+    equipment.write_excel(conf.data / '냉난방방식.xlsx')
+    rich.print(equipment.filter(pl.col('전기식용량비율') == 1))
+
+
+@app['ami'].command(name='parquet')
+def ami_parquet():
+    conf = _Config()
+    src = conf.raw
+    dst = conf.data
     dst.mkdir(exist_ok=True)
     files = list(src.glob('kcl_*.txt'))
 
@@ -195,11 +230,11 @@ def _ami_plot(lf: pl.LazyFrame):
     return fig
 
 
-@app.command
-def ami_plot(src: Path | None = None, dst: Path | None = None):
+@app['ami'].command(name='plot')
+def ami_plot():
     conf = _Config()
-    src = src or conf.data
-    dst = dst or conf.eda
+    src = conf.data
+    dst = conf.eda
 
     dst.mkdir(exist_ok=True)
 
@@ -223,6 +258,7 @@ class Building(NamedTuple):
     name: str
     area: float
     region: str
+    elect_ratio: float | None
 
     def __str__(self):
         return (
@@ -235,20 +271,36 @@ class Building(NamedTuple):
 class PublicAMI:
     energy: Literal['사용량', '보정사용량'] = '보정사용량'
     institution: str | None = None
+    min_elec_ratio: float | None = None
 
     conf: _Config = dc.field(default_factory=_Config)
+
     _file_bldg: str = '1.기관-주소변환.parquet'
     _file_temp: str = 'temperature.parquet'
+    _file_equipment: str = '냉난방방식.parquet'
 
     def buildings(self):
+        equipment = pl.scan_parquet(self.conf.data / self._file_equipment).select(
+            '기관ID', '전기식용량비율'
+        )
         lf = (
             pl.scan_parquet(self.conf.data / self._file_bldg)
+            .with_columns(
+                pl.when(pl.col('기관대분류').str.starts_with('국립대학병원'))
+                .then(pl.lit('국립대학병원 등'))
+                .otherwise(pl.col('기관대분류'))
+                .alias('기관대분류'),
+            )
             .with_row_index()
             .with_columns(pl.col('index').cast(pl.String).str.pad_start(4, '0'))
+            .join(equipment, on='기관ID', how='left')
         )
 
         if self.institution:
             lf = lf.filter(pl.col('기관대분류') == self.institution)
+        if self.min_elec_ratio is not None:
+            lf = lf.filter(pl.col('전기식용량비율') >= self.min_elec_ratio)
+
         return lf
 
     def building_count(self) -> int:
@@ -257,7 +309,9 @@ class PublicAMI:
     def iter_building(self):
         for row in (
             self.buildings()
-            .select('index', '기관ID', '기관명', '연면적', 'asos_code')
+            .select(
+                'index', '기관ID', '기관명', '연면적', 'asos_code', '전기식용량비율'
+            )
             .sort('asos_code')
             .collect()
             .iter_rows()
@@ -281,36 +335,40 @@ class PublicAMI:
     def data(
         self,
         building: Building,
-        temperature: pl.DataFrame | None = None,
+        *,
+        temperature: pl.DataFrame | bool = True,
         group_by_dynamic: str | None = None,
     ):
-        # XXX LocalOutlierFactor 이상치 제거?
-        temp = (
-            self.temperature(building.region).collect()
-            if temperature is None
-            else temperature
-        )
+        if temperature is True:
+            temp = self.temperature(building.region).collect()
+        elif temperature is False:
+            temp = None
+        else:
+            temp = temperature
+
         ami = (
             self.ami(building.id_)
+            .collect()
             .select(
                 'datetime',
                 pl.col(self.energy).truediv(building.area).alias('energy'),
             )
-            .collect()
         )
 
-        data = (
-            ami.join(temp, on='datetime', how='inner')
-            .drop_nulls()
-            .select(
-                'datetime',
-                pl.col('datetime').dt.year().alias('year'),
-                pl.col('datetime').dt.month().alias('month'),
-                pl.col('datetime').dt.weekday().is_in([6, 7]).alias('is_weekend'),
-                pl.col('datetime').dt.hour().alias('hour'),
-                'temperature',
-                'energy',
-            )
+        data = ami
+        if temp is not None:
+            data = ami.join(temp, on='datetime', how='inner')
+            values = ['temperature', 'energy']
+        else:
+            values = ['energy']
+
+        data = data.drop_nulls().select(
+            'datetime',
+            pl.col('datetime').dt.year().alias('year'),
+            pl.col('datetime').dt.month().alias('month'),
+            pl.col('datetime').dt.weekday().is_in([6, 7]).alias('is_weekend'),
+            pl.col('datetime').dt.hour().alias('hour'),
+            *values,
         )
 
         if group_by_dynamic:
@@ -320,7 +378,7 @@ class PublicAMI:
                     every=group_by_dynamic,
                     group_by=['year', 'month', 'is_weekend'],
                 )
-                .agg(pl.mean('temperature', 'energy'))
+                .agg(pl.mean('temperature'), pl.sum('energy'))
                 .with_columns()
             )
 
@@ -357,18 +415,42 @@ class Palettes:
 
 
 @dc.dataclass
-class PublicAmiCpr:
-    ami: PublicAMI
-
+class PublicAmiCprConf:
     period: Literal['daily', 'hourly', 'daytime'] = 'daily'
     daytime_bound: tuple[int, int] = (9, 18)
-    week: Literal['weekday', 'weekend'] | None = None
+    week: Literal['weekday', 'weekend'] | None = 'weekday'
+
+    year: int | None = None
+    min_energy: float = 0.001
+    min_elect: float | None = None
 
     plot: bool = False
-    plot_share_lim: bool = True
+    plot_share_lim: bool = False
+
+    def suffix(self):
+        year = '' if self.year is None else f'{self.year} '
+
+        match self.period:
+            case 'daily':
+                period = '1일'
+            case 'hourly':
+                period = '1시간'
+            case 'daytime':
+                period = f'낮({self.daytime_bound[0]}-{self.daytime_bound[1]})'
+
+        week = {None: '', 'weekday': ' 주중', 'weekend': ' 주말'}[self.week]
+        min_elect = '' if self.min_elect is None else f'_MinElec{self.min_elect}'
+
+        return f'{year}{period}{week}{min_elect}'
+
+
+@dc.dataclass
+class PublicAmiCpr:
+    ami: PublicAMI
+    conf: PublicAmiCprConf
 
     style: cpr.PlotStyle = dc.field(  # type: ignore[assignment]
-        default_factory=lambda: {'scatter': {'s': 10, 'alpha': 0.6}}
+        default_factory=lambda: {'scatter': {'s': 10, 'alpha': 0.25}}
     )
     palettes: Palettes = dc.field(default_factory=Palettes)
 
@@ -378,35 +460,25 @@ class PublicAmiCpr:
     def update(self):
         if 'scatter' not in self.style:
             self.style['scatter'] = {}
-        self.style['scatter']['hue'] = 'month' if self.period == 'daily' else 'hour'
-        self.style['scatter']['palette'] = self.palettes.to_mpl(
-            'sequential' if self.period == 'daytime' else 'cyclic'
+        self.style['scatter']['hue'] = (
+            'month' if self.conf.period == 'daily' else 'hour'
         )
+        self.style['scatter']['palette'] = self.palettes.to_mpl(
+            'sequential' if self.conf.period == 'daytime' else 'cyclic'
+        )
+        self.ami.min_elec_ratio = self.conf.min_elect
         return self
-
-    def suffix(self):
-        match self.period:
-            case 'daily':
-                period = '1일 간격'
-            case 'hourly':
-                period = '1시간 간격'
-            case 'daytime':
-                period = f'낮(({self.daytime_bound[0]}-{self.daytime_bound[1]}))'
-
-        week = {None: '', 'weekday': ' 주중', 'weekend': ' 주말'}[self.week]
-
-        return f'{period}{week}'
 
     def path(self, building: Building):
         return (
             self.ami.conf.cpr / f'{building.idx}_{building.name}_'
-            f'{self.ami.energy}_{self.suffix()}.ext'
+            f'{self.ami.energy}_{self.conf.suffix()}.ext'
         )
 
     def _data(self, building: Building):
         return self.ami.data(
             building=building,
-            group_by_dynamic='1d' if self.period == 'daily' else None,
+            group_by_dynamic='1d' if self.conf.period == 'daily' else None,
         )
 
     def cpr(self, building: Building, data: pl.DataFrame | None = None):
@@ -416,17 +488,24 @@ class PublicAmiCpr:
         if data is None:
             data = self._data(building)
 
-        if self.week is not None:
+        conf = self.conf
+
+        if conf.min_energy:
+            data = data.filter(pl.col('energy') >= conf.min_energy)
+        if conf.year is not None:
+            data = data.filter(pl.col('year') == conf.year)
+
+        if conf.week is not None:
             data = data.filter(
-                pl.when(self.week == 'weekend')
+                pl.when(conf.week == 'weekend')
                 .then(pl.col('is_weekend'))
                 .otherwise(pl.col('is_weekend').not_())
             )
-        if self.period == 'daytime':
-            data = data.filter(pl.col('hour').is_between(*self.daytime_bound))
+        if conf.period == 'daytime':
+            data = data.filter(pl.col('hour').is_between(*conf.daytime_bound))
 
-        if not data.height:
-            raise EmptyDataError
+        if not data.height > 3:  # noqa: PLR2004
+            raise cpr.NotEnoughDataError
 
         return cpr.ChangePointRegression(data).optimize_multi_models()
 
@@ -442,13 +521,13 @@ class PublicAmiCpr:
         model.plot(ax=ax, style=self.update().style)
 
         ax.update_datalim([[0, 0]], updatex=False, updatey=True)
-        if self.plot_share_lim:
+        if self.conf.plot_share_lim:
             data = self._data(building).select('temperature', 'energy').to_numpy()
-            ax.update_datalim(data)
+            ax.update_datalim(data, updatey=False)
         ax.autoscale_view()
 
         ax.set_title(
-            f'{building.name} ({self.suffix()}) r²={model.model_dict["r2"]:.4g}',
+            f'{building.name} ({self.conf.suffix()}) r²={model.model_dict["r2"]:.4g}',
             loc='left',
             weight=500,
         )
@@ -457,7 +536,7 @@ class PublicAmiCpr:
 
         handles, labels = ax.get_legend_handles_labels()
 
-        if self.period == 'daily':
+        if self.conf.period == 'daily':
             labels = [f'{x}월' for x in labels]
         else:
             labels = [f'{x:0>2}:00' for x in labels]
@@ -486,7 +565,7 @@ class PublicAmiCpr:
 
         model = self.cpr(building=building, data=data)
 
-        if self.plot:
+        if self.conf.plot:
             fig, ax = plt.subplots()
             self.plot_cpr(building=building, model=model, ax=ax)
             fig.savefig(path.with_suffix('.png'))
@@ -498,12 +577,12 @@ class PublicAmiCpr:
         dfs: list[pl.DataFrame] = []
 
         for bldg, data in self.ami.track():
-            logger.info(bldg)
+            logger.debug(bldg)
 
             try:
                 df = self.save_cpr(building=bldg, data=data, skip_exists=skip_exists)
-            except EmptyDataError:
-                logger.warning('Empty Data')
+            except (cpr.CprError, ZeroDivisionError) as e:
+                logger.error(f'{e!r} | {bldg}')
                 continue
 
             if df is not None:
@@ -516,57 +595,58 @@ class PublicAmiCpr:
                     )
                 )
 
-        pl.concat(dfs).write_excel(
-            self.ami.conf.cpr / f'[model] {self.ami.energy}_{self.suffix()}.xlsx'
+        df = pl.concat(dfs)
+        path = (
+            self.ami.conf.cpr / f'[model] {self.ami.institution or "전체"} '
+            f'{self.ami.energy}_{self.conf.suffix()}.ext'
         )
+        df.write_parquet(path.with_suffix('.parquet'))
+        df.write_excel(path.with_suffix('.xlsx'))
 
 
-@app.command
-def public_cpr(  # noqa: PLR0913
+DEFAULT_CPR_CONF = PublicAmiCprConf()
+
+
+@app['cpr'].command(name='analyze')
+def cpr_analyze(
     energy: Literal['사용량', '보정사용량'] = '사용량',
-    institution: Annotated[str, Parameter(negative='--all')] = '정부청사관리',
+    institution: str | None = '정부청사관리',
     *,
-    period: Literal['daily', 'hourly', 'daytime'] = 'daily',
-    week: Literal['weekday', 'weekend', 'all'] = 'weekday',
-    daytime_bound: tuple[int, int] = (9, 18),
-    plot: bool = False,
+    all_institution: bool = False,
+    conf: Annotated[PublicAmiCprConf, Parameter(name='conf')] = DEFAULT_CPR_CONF,
 ):
+    if all_institution:
+        institution = None
+
     ami = PublicAMI(energy=energy, institution=institution)
-
-    pa = PublicAmiCpr(
-        ami=ami,
-        period=period,
-        daytime_bound=daytime_bound,
-        week=None if week == 'all' else week,
-        plot=plot,
-    )
-
+    pa = PublicAmiCpr(ami=ami, conf=conf)
     pa.ami.conf.cpr.mkdir(exist_ok=True)
     pa.batch_cpr()
 
 
-@app.command
-def public_cpr_batch(
+@app['cpr'].command(name='batch-analyze')
+def cpr_batch_analyze(
     energy: Literal['사용량', '보정사용량'] = '사용량',
-    institution: Annotated[str, Parameter(negative='--all')] = '정부청사관리',
+    institution: str | None = '정부청사관리',
     *,
-    plot: bool = False,
+    all_institution: bool = False,
+    conf: Annotated[PublicAmiCprConf, Parameter(name='conf')] = DEFAULT_CPR_CONF,
 ):
     from itertools import product  # noqa: PLC0415
+
+    if all_institution:
+        institution = None
 
     for period, week in product(['daily', 'hourly', 'daytime'], ['weekday', 'weekend']):
         logger.info(f'{period=} | {week=}')
 
-        public_cpr(
-            energy=energy,
-            institution=institution,
-            period=period,  # type: ignore[arg-type]
-            week=week,  # type: ignore[arg-type]
-            plot=plot,
-        )
+        conf.period = period  # type:ignore[assignment]
+        conf.week = week  # type:ignore[assignment]
+        cpr_analyze(energy=energy, institution=institution, conf=conf)
 
 
 if __name__ == '__main__':
     utils.LogHandler.set()
     utils.MplTheme('paper').grid().apply()
+
     app()
