54b5cab9e1cc41419e7b475c4c6954dfb285e9c4
beskep <beskep@naver.com>
2024-12-16 11:38:22 +0900

    feat: app remove prefix name_transform

--- greenbutton/utils/app.py
+++ greenbutton/utils/app.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import enum
 import itertools
 from typing import TYPE_CHECKING, Any
 
@@ -7,17 +8,7 @@ import attrs
 import cyclopts
 
 if TYPE_CHECKING:
-    from collections.abc import Iterable
-
-    from cyclopts.core import T
-
-
-class _Sentinel:
-    def __init__(self, v: str, /):
-        self.v = v
-
-    def __repr__(self):
-        return f'<{self.v}>'
+    from collections.abc import Callable, Iterable
 
 
 def _is_helper(name: str | Iterable[str] | None) -> bool:
@@ -32,18 +23,34 @@ def _is_helper(name: str | Iterable[str] | None) -> bool:
     return any(n in helpers for n in name)
 
 
-REGISTERED_ORDER = _Sentinel('REGISTERED_ORDER')
+class RegisteredOrder(enum.Enum):
+    token = 0
+
+
+class RemovePrefix(enum.Enum):
+    token = 0
+
+
+REGISTERED_ORDER = RegisteredOrder.token
+REMOVE_PREFIX = RemovePrefix.token
 
 
 @attrs.define
 class App(cyclopts.App):
     _count: itertools.count = attrs.field(factory=itertools.count)
 
-    def command(
+    def _remove_prefix(self, s: str):
+        if name := self.name:
+            s = s.removeprefix(name[0])
+
+        return cyclopts.default_name_transform(s)
+
+    def command(  # type: ignore[override]
         self,
-        obj: T | None = None,
+        obj: Callable | None = None,
         name: str | Iterable[str] | None = None,
         sort_key: Any = REGISTERED_ORDER,
+        name_transform: Callable[[str], str] | RemovePrefix | None = REMOVE_PREFIX,
         **kwargs: object,
     ):
         if _is_helper(name):
@@ -54,6 +61,10 @@ class App(cyclopts.App):
         if isinstance(obj, cyclopts.App):
             obj._sort_key = sort_key  # noqa: SLF001
         else:
+            if name_transform is REMOVE_PREFIX:
+                name_transform = self._remove_prefix
+
             kwargs['sort_key'] = sort_key
+            kwargs['name_transform'] = name_transform
 
         return super().command(obj=obj, name=name, **kwargs)
