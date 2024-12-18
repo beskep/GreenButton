f0bb22df63ae555c3eef696130d89145c0e54997
beskep <beskep@naver.com>
2024-12-18 13:31:09 +0900

    feat: 예천청사 직접 다운로드 데이터 분석

--- scripts/exp/e04yecheon_gov.py
+++ scripts/exp/e04yecheon_gov.py
@@ -1,19 +1,40 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
-from pathlib import Path  # noqa: F401
-from typing import Annotated
+import dataclasses as dc
+from pathlib import Path
+from typing import TYPE_CHECKING, Annotated
 
 import cyclopts
+import matplotlib.pyplot as plt
+import polars as pl
+import seaborn as sns
+from loguru import logger
 
 import scripts.exp.experiment as exp
 from greenbutton import utils
 from greenbutton.utils import App
 
+if TYPE_CHECKING:
+    from matplotlib.axes import Axes
 
-@dataclass
+
+@dc.dataclass
+class DBDirs:
+    raw: Path = Path('0000.raw')
+    binary: Path = Path('0001.binary')
+    sample: Path = Path('0002.sample')
+
+
+@dc.dataclass
 class Config(exp.BaseConfig):
     BUILDING = 'yecheon_gov'
+
+    db_dirs: DBDirs = dc.field(default_factory=DBDirs)
+
+    def __post_init__(self):
+        for field in (f.name for f in dc.fields(self.db_dirs)):
+            p = getattr(self.db_dirs, field)
+            setattr(self.db_dirs, field, self.dirs.database / p)
 
 
 ConfigParam = Annotated[Config, cyclopts.Parameter(name='*')]
@@ -42,7 +63,90 @@ def sensor_plot(*, conf: ConfigParam, pmv: bool = True, tr7: bool = True):
     exp.plot_sensors(pmv=pmv, tr7=tr7)
 
 
+app.command(App('downloaded', help='설비 컴퓨터에서 직접 다운받은 데이터'))
+
+
+@app['downloaded'].command
+def downloaded_convert(*, conf: ConfigParam):
+    src = conf.db_dirs.raw / '모니터 컴퓨터 다운로드'
+    dst = conf.db_dirs.binary
+
+    def read(path):
+        return (
+            pl.read_excel(path)
+            .rename({'날짜': 'datetime'})
+            .with_columns(
+                pl.format('{}:00', 'datetime').str.to_datetime().alias('datetime')
+            )
+            .unpivot(index='datetime')
+            .with_columns(pl.col('value').cast(pl.Float64))
+        )
+
+    for path in src.glob('*'):
+        if not path.is_dir():
+            continue
+
+        logger.info(path.name)
+
+        data = pl.concat(read(x) for x in path.glob('*.xlsx'))
+        data.write_parquet(dst / f'downloaded-{path.name}.parquet')
+
+
+@app['downloaded'].command
+def downloaded_plot(*, conf: ConfigParam):
+    utils.MplTheme('paper').grid().apply()
+    ax: Axes
+
+    for path in conf.db_dirs.binary.glob('downloaded-*.parquet'):
+        data = (
+            pl.scan_parquet(path)
+            .filter(pl.col('variable').str.starts_with('[1일전]').not_())
+            .with_columns(
+                pl.col('variable')
+                .str.replace('㎥', 'm³')
+                .str.extract_groups(r'(?<group>(\(\S+\))|(\S+ -))?(?<variable>.*)')
+            )
+            .unnest('variable')
+            .with_columns(
+                pl.col('group').str.strip_chars('()- '),
+                pl.col('variable').str.strip_chars(),
+            )
+            .sort('group', 'variable', 'datetime')
+            .with_columns(pl.col('group').fill_null('Null'))
+            .collect()
+        )
+
+        grid = (
+            sns.FacetGrid(
+                data,
+                col='group',
+                col_wrap=int(
+                    utils.ColWrap(data.select(pl.col('group').n_unique()).item())
+                ),
+                sharey=False,
+                height=4,
+                despine=False,
+            )
+            .map_dataframe(
+                sns.lineplot, x='datetime', y='value', hue='variable', alpha=0.75
+            )
+            .set_xlabels('')
+            .set_ylabels('')
+        )
+
+        for ax in grid.axes.ravel():
+            ax.legend()
+            ax.set_yscale('asinh')
+            ax.autoscale_view()
+
+        grid.savefig(conf.dirs.analysis / f'{path.stem}.png')
+        plt.close(grid.figure)
+
+
+app.command(App('db', help='MySQL 백업 데이터'))  # TODO
+
 if __name__ == '__main__':
+    utils.LogHandler.set()
     utils.MplConciseDate().apply()
     utils.MplTheme(palette='tol:vibrant').grid().apply()
 
